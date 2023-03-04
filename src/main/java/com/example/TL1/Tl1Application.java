6d829dd2c8bb91145925a4c62631b3a99cd7f94b
Ganesh0987654321 <ganeshshirsath60@gmail.com>
2023-03-04 17:05:43 +0530

    2nd attempt

---
+++ src/main/java/com/example/TL1/Tl1Application.java
@@ -0,0 +1,14 @@
+package com.example.TL1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Tl1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Tl1Application.class, args);
+		System.out.println("Welcome to 2nd attempt");
+	}
+
+}
